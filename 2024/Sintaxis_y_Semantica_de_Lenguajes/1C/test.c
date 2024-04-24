f57359c5d284f66c3c053d3b24d4f8d2997afd77
ratadesanja <103476368+ratadesanja@users.noreply.github.com>
2024-04-24 20:48:23 -0300

    Automated commit

--- 2024/Sintaxis_y_Semantica_de_Lenguajes/1C/test.c
+++ 2024/Sintaxis_y_Semantica_de_Lenguajes/1C/test.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <Windows.h>
+
+void draw(char c)
+{
+    printf("\x1b[H");
+    for (int i = 0; i < 240; i++)
+        putchar(i % 5 ? ' ' : c);
+    
+    printf("\n");
+    printf("\n");
+}
 int main(void)
 {
-    printf("10");
+    printf("\x1b[2J");
+    draw('o');
     return 0;
 }
